bfa4a3b2b6e5246b2803ff95b9c4fc5b8c10f699
Hector Riquelme <hectorariquelmec@gmail.com>
2021-11-09 22:24:33 -0300

    js added

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const submitButton = document.querySelector("#idSubmit");
+
+submitButton.addEventListener("click",(e)=>{
+    e.preventDefault();
+    alert("Funcionalidad en desarrollo, lamentamos las molestias");
+});
